ced9e60cc929c4c515aadacc005caef0682ddaf4
santiagoceli <santiago.celisc@gmail.com>
2023-01-05 15:08:51 -0500

    Menu fusion in mobile

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
-const navBar = document.querySelector('.navbar-email');
+const navEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuLogo = document.querySelector('.menu');
+const mobilepMenu = document.querySelector('.mobile-menu');
 
-navBar.addEventListener('click', toggleNavbar);
+navEmail.addEventListener('click', toggleDesktopBar);
+menuLogo.addEventListener('click', toggleMobileBar);
 
-function toggleNavbar(){
+function toggleDesktopBar(){
     console.log("trying");
     desktopMenu.classList.toggle('inactive');
 };
+function toggleMobileBar() {
+    console.log('trying2.0');
+    mobilepMenu.classList.toggle('inactive');
+}
 
 // object.style.display = 'value';
